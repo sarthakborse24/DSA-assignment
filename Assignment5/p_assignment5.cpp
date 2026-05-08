0e0cf30e1ff962abd8e23fecd614c0833381289f
sarthakborse24 <sarthakborse98@gmail.com>
2026-05-08 22:58:24 +0530

    function to create tree from postfix added

---
+++ Assignment5/p_assignment5.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <cctype>
+using namespace std;
+
+class Node
+{
+public:
+    char data;
+    Node *left, *right;
+
+    Node(char val)
+    {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+class expressiontree{
+public:    
+    Node *root;
+
+    expressiontree(){
+        root = NULL;
+    }
+
+    Node* createpostfix(char postfix[]){
+
+        Node *stack[100];
+        int Top = -1;
+
+        for(int i=0 ; postfix != '\0'; i++){
+
+            char token = postfix[i];
+            Node * newnode = new Node(token);
+
+            if(isalnum(token)){
+                stack[++Top] = newnode;
+            }
+            else{
+                newnode->right = stack[Top--];
+                newnode->left = stack[Top--];
+
+                stack[++Top] = newnode;
+            }
+        }
+        root = stack[Top];
+        return root;
+    }
+
+    
+};
