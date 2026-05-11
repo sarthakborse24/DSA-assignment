a4cebf176b63c4ba22b5b6f4bba244757d8a6188
sarthakborse24 <sarthakborse98@gmail.com>
2026-05-11 12:01:52 +0530

    --

--- Assignment6/BST.cpp
+++ Assignment6/BST.cpp
@@ -1,55 +1,67 @@
 #include <iostream>
 using namespace std;
 
-class Node{
+class Node
+{
 public:
     int data;
-    Node *left,*right;
-    Node(int val){
+    Node *left, *right;
+    Node(int val)
+    {
         data = val;
         left = right = NULL;
     }
 };
 
-class BST{
+class BST
+{
 public:
     Node *root;
 
-    BST(){
+    BST()
+    {
         root = NULL;
     }
 
-    Node *insert(Node *root , int val){
+    Node *insert(Node *root, int val)
+    {
         if (root == NULL)
         {
             return new Node(val);
         }
 
-        if(root->data > val){
-            root->left = insert(root->left , val);
+        if (root->data > val)
+        {
+            root->left = insert(root->left, val);
         }
-        else if(root->data < val){
-            root->right = insert(root->right , val);
+        else if (root->data < val)
+        {
+            root->right = insert(root->right, val);
         }
         return root;
     }
 
-    void searchkey(Node *root , int key){
-        if(root == NULL)
-            cout<<"not found";
+    void searchkey(Node *root, int key)
+    {
+        if (root == NULL)
+            cout << "not found";
 
-        if(root->data == key){
-            cout<<"element found!!";
-        }    
-        else if(root->data > key){
-            searchkey(root->left , key);
+        if (root->data == key)
+        {
+            cout << "element found!!";
         }
-        else{
-            searchkey(root->right , key);
+        else if (root->data > key)
+        {
+            searchkey(root->left, key);
+        }
+        else
+        {
+            searchkey(root->right, key);
         }
     }
 
-    Node* minivalue(Node *root){
+    Node *minivalue(Node *root)
+    {
         Node *current = root;
 
         while (current && current->left != NULL)
@@ -59,21 +71,26 @@ public:
         return current;
     }
 
-    Node *deletenode(Node *root , int key){
-        if(root == NULL){
+    Node *deletenode(Node *root, int key)
+    {
+        if (root == NULL)
+        {
             return root;
         }
-        if(key < root->data){
-            root->left = deletenode(root->left , key);
+        if (key < root->data)
+        {
+            root->left = deletenode(root->left, key);
         }
         else if (key < root->data)
         {
-            root->right = deletenode(root->right , key);
+            root->right = deletenode(root->right, key);
         }
         // found the element
-        else{
+        else
+        {
             // one child
-            if(root->left == NULL){
+            if (root->left == NULL)
+            {
                 Node *temp = root->right;
                 delete root;
                 return temp;
@@ -90,13 +107,21 @@ public:
             Node *temp = minivalue(root->right);
             root->data = temp->data;
 
-            root->right = deletenode(root->right , temp->data);
-            
+            root->right = deletenode(root->right, temp->data);
         }
-        
+    }
+
+    void Display(Node *root)
+    {
+        if (root != NULL)
+        {
+            Display(root->left);
+            cout << root->data << " ";
+            Display(root->right);
+        }
     }
 };
 
-int main(){
-
+int main()
+{
 }
